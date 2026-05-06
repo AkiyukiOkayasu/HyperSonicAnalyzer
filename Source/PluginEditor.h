877a928ee03608fc4e44ab8911acbced286523c3
AkiyukiOkayasu <akiyuki.okayasu@gmail.com>
2026-05-06 18:45:26 +0900

    Add plugin editor with amplitude range dial controls

---
+++ Source/PluginEditor.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "PluginProcessor.h"
+#include "SpectralAnalyzer.h"
+
+class HyperSonicAnalyzerEditor : public juce::AudioProcessorEditor
+{
+public:
+    explicit HyperSonicAnalyzerEditor(HyperSonicAnalyzerProcessor&);
+    ~HyperSonicAnalyzerEditor() override;
+
+    void paint(juce::Graphics&) override;
+    void resized() override;
+
+private:
+    HyperSonicAnalyzerProcessor& audioProcessor;
+    
+    SpectralAnalyzer spectralAnalyzer;
+    
+    // 振幅範囲設定用ダイヤル
+    juce::Slider minDbSlider;
+    juce::Slider maxDbSlider;
+    
+    juce::Label minDbLabel;
+    juce::Label maxDbLabel;
+    
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> minDbAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> maxDbAttachment;
+    
+    // タイトルラベル
+    juce::Label titleLabel;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(HyperSonicAnalyzerEditor)
+};
